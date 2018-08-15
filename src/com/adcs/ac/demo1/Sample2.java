059ecfeb922345731dce076f9c50cbdc7552f694
Chatlapalli <A712218@corp.alldata.net>
2018-08-14 23:51:30 -0400

    commit2

---
+++ src/com/adcs/ac/demo1/Sample2.java
@@ -0,0 +1,10 @@
+package com.adcs.ac.demo1;
+
+public class Sample2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+System.out.println("hhhhh");
+    }
+
+}
